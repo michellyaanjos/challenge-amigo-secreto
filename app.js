730b8961371cf39020009fcb7501442f7d1457f1
Michelly Anjos <michellyaanjos@gmail.com>
2025-08-10 22:48:03 -0300

    atualizando lista de amigos

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-// Array para armazenar os nomes
-let amigos = []
+// 1. Array para armazenar os nomes
+let amigos = [];
 
-// Função que captura o valor do campo de entrada, para obter o texto inserido pelo usuário
+// 2. Função que captura o valor do campo de entrada, para obter o texto inserido pelo usuário
 function adicionarAmigo() {
-    let nomes = document.getElementById('amigo').value;
-    console.log(nomes);
+    let nome = document.getElementById('amigo').value;
+    console.log(nome);
 
-    // Garantindo que o campo não esteja vazio
-    if (nomes === '') {
+    // 3. Garantindo que o campo não esteja vazio
+    if (nome === '') {
         alert('Por favor, insira um nome.');
-    // Atualizando o array amigos para armazenar os nomes
-    } else {
-        amigos.push(nomes);
-        console.log(amigos);
-        // Limpar campo de entrada
-        limparCampo()
-    }
+        return;
+    } 
+    
+    // 4. Atualizando o array amigos para armazenar os nomes
+    amigos.push(nome); // Adiciona a lista
+    console.log(amigos); // Para que eu consiga visualizar no console
+    exibirListaNaTela();// 8. Exibe uma lista para o usuário na tela
+
+    limparCampo(); // 6. Limpar campo de entrada
+
 }
 
-// Função para limpar campo de entrada
+// 5. Função para limpar campo de entrada
 function limparCampo() {
-    nomes = document.getElementById('amigo');
-    nomes.value = '';
+    nome = document.getElementById('amigo');
+    nome.value = '';
 }
+
+// 7. Função para atualizar e exibir lista de amigos
+function exibirListaNaTela() {
+    let elementoLista = document.getElementById("listaAmigos");
+    elementoLista.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        let amigoNaLista = document.createElement('li');
+
+        amigoNaLista.textContent = amigo;
+
+        elementoLista.appendChild(amigoNaLista);
+    });
+    
+}
+
+
+
+
+
+
+
 
 
 
